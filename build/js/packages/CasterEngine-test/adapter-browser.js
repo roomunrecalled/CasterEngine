f06e32f457be8e347aa350507bc1f8873321b55a
M. Y. Horng <yhorng@zoho.com>
2022-12-20 05:16:14 +0000

    Remove ignored files

--- build/js/packages/CasterEngine-test/adapter-browser.js
+++
@@ -1,2 +0,0 @@
-require("/workspace/CasterEngine/build/js/packages_imported/kotlin-test-js-runner/1.7.20/kotlin-test-karma-runner.js")
-module.exports = require("/workspace/CasterEngine/build/js/packages/CasterEngine-test/kotlin/CasterEngine-test.js")
